7632baa479dd2d7364fafd8ba2c7fdc2c28821a0
chimmy <143209568+lulugabi@users.noreply.github.com>
2025-04-25 23:04:27 -0300

    Create  script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessibil
+
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessit
+
+botaoDeAcessibilidade.addEventListener('click', function (){
+
+cessibilidade.classList.toggle('rotacao-botao');
+
+opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-exp botaoDeAcessibilidade.setAttribute('aria-expanded', ! botaoSelecionado
+
+})
+
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+const diminui FonteBotao = document.getElementById('diminuir-fonte');
+
+const alternaContraste = document.getElementById('alterna-contraste')
+
+let tamanhoAtualFonte = 1;
+
+aumentaFonteBotao.addEventListener('click', function(){
+
+tamanhoAtualFonte += 0.1;
+
+document.body.style.fontSize = `${tamanhoAtualFonte} rem`4
+})
+
+diminuiFonteBotao.addEventListener('click', function(){
+
+tamanhoAtualFonte = 0.1;
+
+document.body.style.fontSize = `${tamanhoAtualFonte} rem`
+
+})
+
+alternaContraste.addEventListener('click', function(){ document.body.classList.toggle('alto-contraste') })
+
+})
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+
+ScrollReveal().reveal('#galeria', { delay: 500 });
+
+ScrollReveal().reveal('#contato', { delay: 500 });
